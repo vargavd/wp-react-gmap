08183085ad89207aa5313e403a5c0769807688fd
vargavd <vvd007@gmail.com>
2022-02-22 20:19:28 +0100

    Seting up google map wrapper

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,37 @@
 // react imports
 import * as React from "react";
 
+// google maps wrapper
+import { Wrapper as GMapWrapper, Status } from "@googlemaps/react-wrapper";
+
+import { GOOGLE_MAP_API_KEY } from "../sensitive";
 
 const App = () => {
+  // states
+  const [map, setMap] = React.useState<google.maps.Map>();
+
+  // ref
+  const mapRef = React.useRef<HTMLDivElement>(null);
+
+  // effects
+  React.useEffect(() => {
+    if (mapRef.current && !map) {
+      setMap(new google.maps.Map(mapRef.current, {}));
+    }
+  }, [mapRef, map]);
+
+
+  // helper funcs
+  const renderGMapWrapperStatus = (status: Status) => {
+    return (
+      <h2>{status}</h2> 
+    );
+  };
+
   return (
-    <h1>
-      Hello from App component - 1723
-    </h1>
+    <GMapWrapper apiKey={GOOGLE_MAP_API_KEY} render={renderGMapWrapperStatus}>
+      <div ref={mapRef} />
+    </GMapWrapper>
   );
 };
 
