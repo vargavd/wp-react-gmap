c86eb240b564cca5ec4a5818feb86f2e5a7632fd
vargavd <vvd007@gmail.com>
2022-02-23 04:02:39 +0100

    Applying some styles to map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -19,7 +19,122 @@ const Map = () => {
     if (mapRef.current) {
       setMap(new window.google.maps.Map(mapRef.current, {
         center: { lat: 0, lng: 0 },
-        zoom: 3
+        zoom: 3,
+        disableDefaultUI: true,
+        zoomControl: true,
+        styles: [
+          {
+              "featureType": "all",
+              "elementType": "labels.text",
+              "stylers": [
+                  {
+                      "color": "#878787"
+                  }
+              ]
+          },
+          {
+              "featureType": "all",
+              "elementType": "labels.text.stroke",
+              "stylers": [
+                  {
+                      "visibility": "off"
+                  }
+              ]
+          },
+          {
+              "featureType": "administrative",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                  {
+                      "visibility": "on"
+                  },
+                  {
+                      "color": "#bdbdbd"
+                  }
+              ]
+          },
+          {
+              "featureType": "administrative",
+              "elementType": "labels",
+              "stylers": [
+                  {
+                      "visibility": "off"
+                  }
+              ]
+          },
+          {
+              "featureType": "administrative.country",
+              "elementType": "labels.text",
+              "stylers": [
+                  {
+                      "visibility": "simplified"
+                  }
+              ]
+          },
+          {
+              "featureType": "landscape",
+              "elementType": "all",
+              "stylers": [
+                  {
+                      "color": "#f9f5ed"
+                  }
+              ]
+          },
+          {
+              "featureType": "landscape",
+              "elementType": "geometry.fill",
+              "stylers": [
+                  {
+                      "visibility": "on"
+                  }
+              ]
+          },
+          {
+              "featureType": "landscape",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                  {
+                      "visibility": "off"
+                  }
+              ]
+          },
+          {
+              "featureType": "landscape.natural",
+              "elementType": "geometry.fill",
+              "stylers": [
+                  {
+                      "visibility": "on"
+                  }
+              ]
+          },
+          {
+              "featureType": "road.highway",
+              "elementType": "all",
+              "stylers": [
+                  {
+                      "color": "#f5f5f5"
+                  }
+              ]
+          },
+          {
+              "featureType": "road.highway",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                  {
+                      "color": "#c9c9c9"
+                  }
+              ]
+          },
+          {
+              "featureType": "water",
+              "elementType": "all",
+              "stylers": [
+                  {
+                      "color": "#aee0f4"
+                  }
+              ]
+          }
+        ],
       }));
     }
   }, [mapRef]);
